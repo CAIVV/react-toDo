fe069e2f64b74faee42d85b1b08d5f37a501cbf5
VelociraptorJS <originsJS@gmail.com>
2017-02-20 15:36:27 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,61 @@ import Header from './components/Header';
 import Todo from './components/Todo'
 import './styles.css';
 
-
-function App(props) {
-	return (
-	<main>
-		<Header title={props.title} />
+class App extends React.Component {
+	constructor(props) {
+		super(props);
 		
-		<section className="todo-list">
-			{props.todos.map(todo =>
-				<Todo key={todo.id} title={todo.title} completed={todo.completed} />)
-			}
-		</section>
-	</main>
-	);
+		this.state = {
+			todos: this.props.initialData
+		};
+		
+		this.handleStatusChange = this.handleStatusChange.bind(this);
+		this.handleDelete = this.handleDelete.bind(this);
+	}
+
+handleStatusChange(id) {
+	let todos = this.state.todos.map(todo => {
+		if(todo.id === id) {
+			todo.completed = !todo.completed;
+		}
+		return todo;
+	});
+	
+	this.setState({todos});
+}	
+
+handleDelete(id) {
+	let todos = this.state.todos.filter(todo => todo.id !==id);
+	
+	this.setState({todos});
+}
+	
+render() {
+	return (
+		<main>
+			<Header title={this.props.title} />
+
+			<section className="todo-list">
+				{this.state.todos.map(todo =>
+					<Todo 
+					key={todo.id} 
+					id={todo.id} 
+					title={todo.title} 
+					completed={todo.completed} 
+					onStatusChange={this.handleStatusChange}
+					onDelete={this.handleDelete}
+					/>)
+				}
+			</section>
+		</main>
+		);
+	}
 }
 
 App.propTypes = {
 	id: React.PropTypes.number,
 	title: React.PropTypes.string,
-	todos: React.PropTypes.arrayOf(React.PropTypes.shape({
+	initialData: React.PropTypes.arrayOf(React.PropTypes.shape({
 		title: React.PropTypes.string.isRequired,
 		completed: React.PropTypes.bool.isRequired
 	})).isRequired
@@ -35,4 +71,4 @@ App.defaultProps = {
 	title: 'React Todo'
 };
 
-ReactDOM.render(<App todos={todos}/>, document.getElementById('root'));
+ReactDOM.render(<App initialData={todos}/>, document.getElementById('root'));
